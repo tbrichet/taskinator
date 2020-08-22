69865091089a414716d01cd6d751b9418996ad1a
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-08-21 22:18:53 -0700

    refractored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var taskFormHandler = function(event) {
 
     event.preventDefault();
-
     var taskNameInput = document.querySelector("input[name='task-name']").value;
-
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    //send as argument to createTaskEl
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function(taskDataObj) {
 
     //create list item
     var listItemEl = document.createElement("li");
@@ -20,7 +30,7 @@ var createTaskHandler = function() {
     taskInfoEl.className = "task-info";
 
     //add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     
     listItemEl.appendChild(taskInfoEl);
 
@@ -28,4 +38,4 @@ var createTaskHandler = function() {
     tasksToDoEl.appendChild(listItemEl);
 }
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
